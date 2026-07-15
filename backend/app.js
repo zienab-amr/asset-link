64d7a6f85824f21eee5edce2b1e26a804782de46
zienab-amr <99207638+zienab-amr@users.noreply.github.com>
2026-07-16 01:05:08 +0300

    Set up Express server with DB and Redis connections
    Initialize server with database and Redis connections.

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+const { connectRedis } = require('./config/redis');
+
+// إعداد التطبيق
 const app = express();
 
 // Middlewares
@@ -8,6 +13,25 @@ app.use(express.json());
 
 // Routes
 const authRoutes = require('./routes/auth.routes');
+
 app.use('/api/auth', authRoutes);
+
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    await connectRedis();
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running successfully on port ${PORT}`);
+    });
+
+  } catch (err) {
+    console.error('Failed to start server:', err);
+    process.exit(1); 
+  }
+};
+startServer();
 
 module.exports = app;
