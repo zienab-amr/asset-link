64c543f58e91d035db18570ca519f5fc6726b00b
AbdallhOmran <171288488+AbdallhOmran@users.noreply.github.com>
2026-07-15 20:47:22 +0300

    feat: initialize backend server with Express, Mongoose, and environment configuration

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-const connectDB = require('./config/db');
+require("dotenv").config();
 
-require('dotenv').config();
+console.log("PORT =", process.env.PORT);
+console.log("MONGO_URI =", process.env.MONGO_URI);
+
+const connectDB = require("./config/db");
 
 connectDB();
 
-app.listen(process.env.PORT, (req,res)=>{
-  console.log(`server in running on port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
