404f5870e9d275808ba77e5eb5c30415bcbb0bda
zienab-amr <99207638+zienab-amr@users.noreply.github.com>
2026-07-21 00:30:03 +0300

    Refactor createPayment function in payment.service.js
    Refactor createPayment function to improve error handling and calculate total payment amount correctly.

--- backend/services/payment.service.js
+++ backend/services/payment.service.js
@@ -1,26 +1,36 @@
-const Payment = require("../models/payment.model")
-const Booking = require("../models/booking.model")
-const contract = require("../models/contract.model")
+const Payment = require("../models/payment.model");
+const Booking = require("../models/booking.model");
+const Contract = require("../models/contract.model"); 
 
-const createPayment =async(bookingId)=>{
-    const booking = await Booking.findById(bookingId)
-    if(!booking){
-        throw new Error("Booking not found")
+const createPayment = async (bookingId) => {
+    const booking = await Booking.findById(bookingId);
+    if (!booking) {
+        throw new Error("Booking not found");
     }
-    if(booking.status !=="Confirmed"){
-        throw new Error("Booking is not confirmed")
+    if (booking.status !== "Confirmed") {
+        throw new Error("Booking is not confirmed");
     }
-    const existingPayment = await Payment.findOne({bookingId})
+    const existingPayment = await Payment.findOne({ bookingId });
     
     if (existingPayment) {
         throw new Error("Booking already paid");
     }
+
+    const contractData = await Contract.findOne({ bookingId });
+    if (!contractData) {
+        throw new Error("Contract not found for this booking");
+    }
+
+    const totalToPay = contractData.totalPrice + contractData.securityDeposit;
+
     const payment = await Payment.create({
-         bookingId: booking._id,
+        bookingId: booking._id,
         companyId: booking.companyId,
-        amount: contract.totalPrice,
+        amount: totalToPay,
         paymentStatus: "Pending",
-    })
-    return payment
+    });
+    
+    return payment;
 }
-module.exports = {createPayment}
+
+module.exports = { createPayment };
