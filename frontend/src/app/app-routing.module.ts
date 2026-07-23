2d890dc177b78d76be647bd067b30d654d4ed6f8
Eng-MinaAshraf <Eng-MinaAshraf@users.noreply.github.com>
2026-07-23 21:25:10 +0300

    feat: add Maintenance Schedule page (table, request modal, predictive alerts)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'maintenance-schedule',
+    loadChildren: () => import('./pages/maintenance-schedule/maintenance-schedule.module').then(m => m.MaintenanceScheduleModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/pages/maintenance-schedule/maintenance-schedule.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../../shared/shared.module';
+
+import { MaintenanceScheduleRoutingModule } from './maintenance-schedule-routing.module';
+import { MaintenanceScheduleComponent } from './maintenance-schedule.component';
+import { MaintenanceStatsComponent } from './components/maintenance-stats/maintenance-stats.component';
+import { PredictiveAlertsComponent } from './components/predictive-alerts/predictive-alerts.component';
+import { MaintenanceTableComponent } from './components/maintenance-table/maintenance-table.component';
+import { RequestMaintenanceModalComponent } from './components/request-maintenance-modal/request-maintenance-modal.component';
+
+@NgModule({
+  declarations: [
+    MaintenanceScheduleComponent,
+    MaintenanceStatsComponent,
+    PredictiveAlertsComponent,
+    MaintenanceTableComponent,
+    RequestMaintenanceModalComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+    MaintenanceScheduleRoutingModule
+  ]
+})
+export class MaintenanceScheduleModule {}
